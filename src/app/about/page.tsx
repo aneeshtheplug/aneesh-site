1dbc1dafbc2ae6ccd5e0b8b8ea69d112f635826c
Aneesh Agarwal <911aneesh@gmail.com>
2024-11-14 18:50:18 -0800

    about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function AboutPage() {
+  return (
+    <div className = "text-red-400">
+      ABOUT ME!
+
+      <Image src = "next.svg" alt = "Next logo" width = {500} height = {500}/>
+,    </div>
+  )
+}
